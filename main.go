e5a08e9018a44d9d7fc23baa3c81bfd1b6971b27
jin4 <kd1192061@st.kobedenshi.ac.jp>
2018-08-14 08:18:49 +0900

    [add] Basic認証

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"studying-golang-echo/handler"
+	"studying-golang-echo/interceptor"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -14,11 +15,11 @@ func main() {
 	// ミドルウェア
 	e.Use(middleware.Logger()) //HTTPのリクエストのログを出力
 	e.Use(middleware.Recover())
+	//e.Use(interceptor.BasicAuth())
 
 	// ルーティング
-	e.GET("/hello", handler.MainPage())
-	e.Get("/hello/:username", handle.MainPage()) //セミコロンの場所がプレースホルダ
-
+	e.GET("/hello", handler.MainPage())                        //文字出力のみ
+	e.GET("/app", handler.MainPage(), interceptor.BasicAuth()) //Basic認証
 	// サーバー起動
 	e.Start(":8000") //ポート番号8000指定
 }
