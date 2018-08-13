0cffb7a4d31a17e5c732e0dc9ab2fb173c3f5115
jin4 <kd1192061@st.kobedenshi.ac.jp>
2018-08-14 03:26:21 +0900

    basic sample

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"studying-golang-echo/handler"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", handler.MainPage())
+
+	// サーバー起動
+	e.Start(":8000") //ポート番号8000指定
+}
